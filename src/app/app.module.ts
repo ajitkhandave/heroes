e4b067ba024c2cea3fd30a2aa8333625d3bd9e60
ajitkhandave <ajitkhandave@gmail.com>
2017-03-07 17:57:58 +0530

    files has been updated after http api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule }   from '@angular/router';
+import { AppRoutingModule }     from './app-routing/app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
@@ -9,7 +13,8 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { AppRoutingModule }     from './app-routing/app-routing.module';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+
 
 RouterModule.forRoot([
   {
@@ -23,15 +28,15 @@ RouterModule.forRoot([
       BrowserModule,
       FormsModule,
        HttpModule,
+       InMemoryWebApiModule.forRoot(InMemoryDataService),
       AppRoutingModule
     ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
-
-
+    HeroSearchComponent
   ],
 
   providers: [HeroService],
